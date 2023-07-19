357632db76ae3491d543e349759305ebc555e407
Rozhina Ghanavi <74327303+rghnn@users.noreply.github.com>
2023-07-18 21:51:03 -0400

    Update bound.py

--- pyddm/models/bound.py
+++ pyddm/models/bound.py
@@ -164,4 +164,4 @@ class BoundCollapsingHyperbolic(Bound):
     @accepts(Self, Positive0)
     @returns(Positive0)
     def get_bound(self, t, *args, **kwargs):
-        return max(1/(self.tau*t+1),0)
+        return 1/(self.tau*t+B)
